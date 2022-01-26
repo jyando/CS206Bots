77848a3fafb427c3a834cda27414e5c0bbdfb30d
Joe Yando <7kzS5t%kOiPg'i^WF%>
2022-01-25 19:22:07 -0500

    Simpliflied the world

--- generate.py
+++ generate.py
@@ -1,6 +1,6 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("boxes.sdf")
+pyrosim.Start_SDF("world.sdf")
 
 length = 1
 width = 1
@@ -9,10 +9,8 @@ height = 1
 x = 0
 y = 0
 z = 0.5
-for k in range(0,5):
-    for j in range(0,5):
-        for i in range(0,10):
-            pyrosim.Send_Cube(name="Box", pos=[x+j, y+k, z+i], size=[(length*(.9**(i+1))), (width*(.9**(i+1))), (height*(.9**(i+1)))])
+
+pyrosim.Send_Cube(name="Box", pos=[x, y, z], size=[length, width, height])
 
 
 pyrosim.End()
--- simulate.py
+++ simulate.py
@@ -10,7 +10,7 @@ p.setGravity(0,0,-9.8)
 
 planeId = p.loadURDF("plane.urdf")
 
-p.loadSDF("boxes.sdf")
+p.loadSDF("world.sdf")
 
 for i in range(0,1000):
     p.stepSimulation()
