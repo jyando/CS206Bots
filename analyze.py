94de738d015ba47e32f15d046bee418241b0ae0c
Joe Yando <7kzS5t%kOiPg'i^WF%>
2022-02-09 17:08:21 -0500

    Graphed the values of sin stored in a vector

--- analyze.py
+++ analyze.py
@@ -3,11 +3,14 @@ import matplotlib.pyplot
 
 filenameBack = 'data/backLegData.npy'
 filenameFront = 'data/frontLegData.npy'
+filenameSin = 'data/sinFunction.npy'
 
 backLegSensorValues = numpy.load(filenameBack)
 frontLegSensorValues = numpy.load(filenameFront)
+sinValues = numpy.load(filenameSin)
 
-matplotlib.pyplot.plot(backLegSensorValues, label = "Back Leg Sensor", linewidth = 2)
-matplotlib.pyplot.plot(frontLegSensorValues, label = " Front Leg Sensor")
+#matplotlib.pyplot.plot(backLegSensorValues, label = "Back Leg Sensor", linewidth = 2)
+#matplotlib.pyplot.plot(frontLegSensorValues, label = " Front Leg Sensor")
+matplotlib.pyplot.plot(sinValues, label = " Front Leg Sensor")
 matplotlib.pyplot.legend()
 matplotlib.pyplot.show()
