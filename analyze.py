4927eeda4e0eaaa1edba698abc58102aa0ca2bd8
Joe Yando <7kzS5t%kOiPg'i^WF%>
2022-02-10 16:56:09 -0500

    Graphed the two different motors angles

--- analyze.py
+++ analyze.py
@@ -3,14 +3,18 @@ import matplotlib.pyplot
 
 filenameBack = 'data/backLegData.npy'
 filenameFront = 'data/frontLegData.npy'
-filenameSin = 'data/sinFunction.npy'
+filenameSinFL = 'data/sinFunctionFL.npy'
+filenameSinBL = 'data/sinFunctionBL.npy'
 
-backLegSensorValues = numpy.load(filenameBack)
-frontLegSensorValues = numpy.load(filenameFront)
-sinValues = numpy.load(filenameSin)
+# backLegSensorValues = numpy.load(filenameBack)
+# frontLegSensorValues = numpy.load(filenameFront)
+sinValuesFL = numpy.load(filenameSinFL)
+sinValuesBL = numpy.load(filenameSinBL)
+
 
 #matplotlib.pyplot.plot(backLegSensorValues, label = "Back Leg Sensor", linewidth = 2)
 #matplotlib.pyplot.plot(frontLegSensorValues, label = " Front Leg Sensor")
-matplotlib.pyplot.plot(sinValues, label = " Front Leg Sensor")
+matplotlib.pyplot.plot(sinValuesFL, label = " Front Leg Sensor", linewidth = 5)
+matplotlib.pyplot.plot(sinValuesBL, label = " Back Leg Sensor")
 matplotlib.pyplot.legend()
 matplotlib.pyplot.show()
