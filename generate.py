201d1ffee567fb3209b608f0ab6af7b04b2f3b5c
Joe Yando <7kzS5t%kOiPg'i^WF%>
2022-03-01 14:02:17 -0500

    Automated the creating of synapses and made a way to randomize the target angles

--- generate.py
+++ generate.py
@@ -1,12 +1,9 @@
 import pyrosim.pyrosim as pyrosim
 import constants as c
+import random
 
 def Create_World():
     pyrosim.Start_SDF("world.sdf")
-
-    # length = 1
-    # width = 1
-    # height = 1
     x = -2.5
     y = 2.5
     z = 0.5
@@ -14,21 +11,6 @@ def Create_World():
     pyrosim.Send_Cube(name="Box", pos=[x, y, z], size=[c.length, c.width, c.height])
 
     pyrosim.End()
-
-#def Create_Robot():
-    # pyrosim.Start_URDF("body.urdf")
-    #
-    # length = 1
-    # width = 1
-    # height = 1
-    #
-    # pyrosim.Send_Cube(name="Torso", pos=[1.5, 0, 1.5], size=[length, width, height])
-    # pyrosim.Send_Joint(name="Torso_BackLeg", parent="Torso", child="BackLeg", type="revolute", position=[1,0,1 ])
-    # pyrosim.Send_Cube(name="BackLeg", pos=[-.5, 0, -.5], size=[length, width, height])
-    # pyrosim.Send_Joint(name="Torso_FrontLeg", parent="Torso", child="FrontLeg", type="revolute", position=[2, 0, 1])
-    # pyrosim.Send_Cube(name="FrontLeg", pos=[.5, 0, -.5], size=[length, width, height])
-    #
-    # pyrosim.End()
 
 def Generate_Body():
     pyrosim.Start_URDF("body.urdf")
@@ -50,10 +32,13 @@ def Generate_Brain():
     pyrosim.Send_Motor_Neuron(name=3, jointName="Torso_BackLeg")
     pyrosim.Send_Motor_Neuron(name=4, jointName="Torso_FrontLeg")
 
-    pyrosim.Send_Synapse(sourceNeuronName=0, targetNeuronName=3, weight=-1.2)
-    pyrosim.Send_Synapse(sourceNeuronName=1, targetNeuronName=3, weight=0.5)
-    pyrosim.Send_Synapse(sourceNeuronName=0, targetNeuronName=4, weight=-0.8)
-    pyrosim.Send_Synapse(sourceNeuronName=1, targetNeuronName=4, weight=-1.0)
+
+    for i in range(3):
+
+        for j in range(3,5):
+            pyrosim.Send_Synapse(sourceNeuronName=i, targetNeuronName=j, weight=(2*random.random())-1)
+            #pyrosim.Send_Synapse(sourceNeuronName=i, targetNeuronName=j, weight=random.random())
+
 
     pyrosim.End()
 
