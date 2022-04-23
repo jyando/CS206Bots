3ee6143a241f2bd94de7de7e512ea57bec10e69e
Joe Yando <7kzS5t%kOiPg'i^WF%>
2022-04-23 13:33:39 -0400

    Simulation Writes out to a csv file

--- search.py
+++ search.py
@@ -2,14 +2,10 @@ import os
 from hillclimber import HILL_CLIMBER
 from parallelHillClimber import PARALLEL_HILL_CLIMBER
 
-# hc = HILL_CLIMBER()
-# hc.Evolve()
-# hc.Show_Best()
-
 phc = PARALLEL_HILL_CLIMBER()
 
 phc.Evolve()
 
-phc.Show_Best()
+phc.WriteOut()
 
-#phc.Delete_Files()
+phc.Show_Best()
