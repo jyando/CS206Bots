610a15aafbe2666bcc18d111ca90a177e99359a8
Joe Yando <7kzS5t%kOiPg'i^WF%>
2022-04-23 13:08:36 -0400

    It writes the fitness values of all the generations to a csv file

--- parallelHillClimber.py
+++ parallelHillClimber.py
@@ -2,6 +2,9 @@ from solution import SOLUTION
 import constants as c
 import copy
 import os
+import numpy
+import pandas
+import csv
 import constants as c
 from pyrosim.neuralNetwork import NEURAL_NETWORK
 
@@ -18,6 +21,10 @@ class PARALLEL_HILL_CLIMBER:
 
         self.nextAvailableID = 0
 
+        self.data = numpy.zeros([c.populationSize, c.numberOfGenerations])
+        self.row = 0
+        self.column = 0
+
         for i in range(c.populationSize):
             self.parents[i] = SOLUTION(self.nextAvailableID)
             self.nextAvailableID += 1
@@ -29,6 +36,7 @@ class PARALLEL_HILL_CLIMBER:
 
         for currentGeneration in range(c.numberOfGenerations):
             self.Evolve_For_One_Generation()
+            self.column += 1
 
     def Evolve_For_One_Generation(self):
         self.Spawn()
@@ -48,27 +56,37 @@ class PARALLEL_HILL_CLIMBER:
             self.nextAvailableID += 1
             self.children[i] = SOLUTION(self.nextAvailableID)
 
+
+
     def Mutate(self):
         for i in range(len(self.children)):
             self.children[i].Mutate()
 
     def Select(self):
 
+        self.row = 0
         for i in range(len(self.parents)):
-            if(self.children[i].fitness < self.parents[i].fitness):
+            if(self.children[i].fitness > self.parents[i].fitness):
                 self.parents[i] = self.children[i]
 
+            self.data[self.row][self.column] = self.parents[i].fitness
+            self.row += 1
+
     def Print(self):
+
         for i in range(len(self.parents)):
             print(" ")
             print("Parent Fitness Value: ", self.parents[i].fitness, " Child Fitness Value: ", self.children[i].fitness)
             print("")
 
+
     def Show_Best(self):
+
+        #print(self.data)
 
         self.bestKey = 0
         for i in range (len(self.parents)):
-            if self.parents[i].fitness < self.parents[self.bestKey].fitness:
+            if self.parents[i].fitness > self.parents[self.bestKey].fitness:
                 self.bestKey = i
 
         self.parents[self.bestKey].Start_Simulation("GUI")
@@ -81,3 +99,16 @@ class PARALLEL_HILL_CLIMBER:
 
         for i in range(len(solutions)):
             solutions[i].Wait_For_Simulation_To_End()
+
+    def WriteOut(self):
+        filename = "testB.csv"
+
+        self.dataPd = pandas.DataFrame(self.data)
+
+        print(self.dataPd)
+        self.dataPd.to_csv(filename, mode='a', index=False, header=False)
+        # writing to csv file
+        # with open(filename, 'w') as csvfile:
+        #     # creating a csv writer object
+        #     csvwriter = csv.writer(csvfile)
+        #     csvwriter.writerow(self.dataPd[1])
